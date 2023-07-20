88345452b66e596bb316d267ca34641cc5c4a646
MC-BHSEC <80209223+MC-BHSEC@users.noreply.github.com>
2023-07-20 09:54:19 -0400

    Add files via upload

---
+++ Section2/intro-cheng2/intro-cheng2/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  intro-cheng2
+//
+//  Created by Mark Cheng on 7/19/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var example: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Section2/mp1-cheng2/mp1-cheng2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mp1-cheng2
+//
+//  Created by Mark Cheng on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
